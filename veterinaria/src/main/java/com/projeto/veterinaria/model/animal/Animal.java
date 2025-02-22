16c5d38266a27e667f4b7abad34f29f4dfe507b4
Felipe Seiji <fenishina@gmail.com>
2025-02-22 18:26:01 -0300

    Atualização animal

--- veterinaria/src/main/java/com/projeto/veterinaria/model/animal/Animal.java
+++ veterinaria/src/main/java/com/projeto/veterinaria/model/animal/Animal.java
@@ -1,6 +1,10 @@
 package com.projeto.veterinaria.model.animal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,11 +17,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Animal {
     //Atributos do animal
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(unique = false, nullable = true, length = 50)
     private String nome;
+
+    @Column(unique = false, nullable = false, length = 50)
     private String especie;
+
+    @Column(unique = false, nullable = false, length = 50)
     private String raca;
+
+    @Column(unique = false, nullable = false, length = 4)
     private int idade;
+
+    @Column(unique = false, nullable = false, length = 15)
     private String sexo;
+
+    @Column(unique = false, nullable = false, length = 3)
     private double peso;
 }
