c098f63532ab31a2050e817b7d706d76bb357ccc
Felipe Seiji <fenishina@gmail.com>
2025-02-27 18:15:13 -0300

    Criação da classe Agendamento

---
+++ veterinaria/src/main/java/com/projeto/veterinaria/model/agendamento/Agendamento.java
@@ -0,0 +1,43 @@
+package com.projeto.veterinaria.model.agendamento;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "agendamento")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Agendamento implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private LocalDate dataAgendamento;
+
+    @Lob
+    @NotBlank 
+    @Size(max = 500)
+    private String descricao;
+
+}
